d274e4894ec7c64840161f6716dfdf99d095ec2a
Aman <amanchourasia.tech@gmail.com>
2020-08-30 14:53:01 +0530

    Leader Election Implementation

--- LeaderElection/src/main/java/com/distributed/LeaderElection.java
+++ LeaderElection/src/main/java/com/distributed/LeaderElection.java
@@ -1,28 +1,57 @@
 package com.distributed;
 
 import java.io.IOException;
+import java.util.Collections;
+import java.util.List;
 
+import org.apache.zookeeper.CreateMode;
+import org.apache.zookeeper.KeeperException;
 import org.apache.zookeeper.WatchedEvent;
 import org.apache.zookeeper.Watcher;
+import org.apache.zookeeper.ZooDefs;
 import org.apache.zookeeper.ZooKeeper;
 
-public class LeaderElection implements Watcher{
-	
+public class LeaderElection implements Watcher {
+
 	private ZooKeeper zooKeeper = null;
+	private String currentZnodeName;
 	private static final String ZOOKEEPER_LOCATION = "localhost:2181";
-	private static final int SESSION_TIMEOUT = 3000;
-	
-	public static void main(String[] args) throws IOException, InterruptedException {
+	private static final int SESSION_TIMEOUT = 3000; // Time after which Zookeeper server will treat client as dead
+	private static final String ELECTION_NAMESPACE = "/election";
+
+	public static void main(String[] args) throws IOException, InterruptedException, KeeperException {
 		LeaderElection election = new LeaderElection();
 		election.connctToZookeeper();
+		election.standInElection();
+		election.electLeader();
 		election.run();
 		election.close();
 		System.out.println("Disconnected from Zookeeper, exiting application.");
 	}
+
+	void standInElection() throws KeeperException, InterruptedException {
+		String zNodePrefix = ELECTION_NAMESPACE + "/c_"; // Here c stands for candidate
+		String zNodeFullPath = zooKeeper.create(zNodePrefix, new byte[] {}, ZooDefs.Ids.OPEN_ACL_UNSAFE,
+				CreateMode.EPHEMERAL_SEQUENTIAL);
+
+		System.out.println("znode name : " + zNodeFullPath);
+		this.currentZnodeName = zNodeFullPath.replace(ELECTION_NAMESPACE + "/", "");
+	}
 	
+	void electLeader() throws KeeperException, InterruptedException {
+		List<String> children = this.zooKeeper.getChildren(ELECTION_NAMESPACE, false);
+		
+		Collections.sort(children);
+		String leaderZnode = children.get(0);
+		if(leaderZnode.equals(currentZnodeName))
+			System.out.println("I am the leader.");
+		else
+			System.out.println("The leader is "+ leaderZnode + " . I am "+ currentZnodeName);
+	}
+
 	private void close() throws InterruptedException {
 		this.zooKeeper.close();
-		
+
 	}
 
 	private void connctToZookeeper() throws IOException {
@@ -32,8 +61,8 @@ public class LeaderElection implements Watcher{
 	public void process(WatchedEvent event) {
 		switch (event.getType()) {
 		case None:
-			if(event.getState() == Event.KeeperState.SyncConnected)
-				System.out.println("Successfully connected to Zookeeper.");	
+			if (event.getState() == Event.KeeperState.SyncConnected)
+				System.out.println("Successfully connected to Zookeeper.");
 			else {
 				synchronized (zooKeeper) {
 					System.out.println("Disconnected from zookeeper event");
@@ -45,14 +74,14 @@ public class LeaderElection implements Watcher{
 		default:
 			break;
 		}
-		
+
 	}
 
 	public void run() throws InterruptedException {
 		synchronized (zooKeeper) {
 			zooKeeper.wait();
 		}
-		
+
 	}
 
 }
