90bf3fb0b3d705d37be729058f33190ffa4c1ec7
Aman <amanchourasia.tech@gmail.com>
2020-08-30 19:39:00 +0530

    Added handlers for triggers

--- WatchersAndTriggers/src/main/java/com/distributed/WatchersAndTriggers.java
+++ WatchersAndTriggers/src/main/java/com/distributed/WatchersAndTriggers.java
@@ -1,19 +1,24 @@
 package com.distributed;
 
 import java.io.IOException;
+import java.util.List;
 
+import org.apache.zookeeper.KeeperException;
 import org.apache.zookeeper.WatchedEvent;
 import org.apache.zookeeper.Watcher;
 import org.apache.zookeeper.ZooKeeper;
+import org.apache.zookeeper.data.Stat;
 
 public class WatchersAndTriggers implements Watcher {
 	private static final int SESSION_TIMEOUT = 3000;
 	private static ZooKeeper zooKeeper;
 	private static final String ZOOKEEPER_ADDRESS = "localhost:2181";
+	private static final String TARGET_ZNODE = "/target_znode";
 
-	public static void main(String[] args) throws IOException, InterruptedException {
+	public static void main(String[] args) throws IOException, InterruptedException, KeeperException {
 		WatchersAndTriggers watchersAndTriggers = new WatchersAndTriggers();
 		watchersAndTriggers.connectToZookeeper();
+		watchersAndTriggers.watchTargetNode();
 		watchersAndTriggers.run();
 		watchersAndTriggers.close();
 
@@ -26,6 +31,16 @@ public class WatchersAndTriggers implements Watcher {
 
 	void connectToZookeeper() throws IOException {
 		this.zooKeeper = new ZooKeeper(ZOOKEEPER_ADDRESS, SESSION_TIMEOUT, this);
+	}
+
+	public void watchTargetNode() throws KeeperException, InterruptedException {
+		Stat stat = this.zooKeeper.exists(TARGET_ZNODE, this);
+		if (stat == null)
+			return;
+		byte[] data = this.zooKeeper.getData(TARGET_ZNODE, this, stat);
+		List<String> children = this.zooKeeper.getChildren(TARGET_ZNODE, this);
+
+		System.out.println("Target Node Data : " + new String(data) + " children are " + children);
 	}
 
 	@Override
@@ -41,8 +56,27 @@ public class WatchersAndTriggers implements Watcher {
 				}
 			}
 			break;
+		case NodeCreated:
+			System.out.println(TARGET_ZNODE + " was created.");
+			break;
+		case NodeDeleted:
+			System.out.println(TARGET_ZNODE + " was deleted.");
+			break;
+		case NodeChildrenChanged:
+			System.out.println(TARGET_ZNODE + " was children changed.");
+			break;
+		case NodeDataChanged:
+			System.out.println(TARGET_ZNODE + " was data changed.");
 		}
-
+		try {
+			watchTargetNode();
+		} catch (KeeperException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 	void run() throws InterruptedException {
